c6153c612e8f5a0d0c4a5f8c235399dea58cf924
philipwerner <philip.r.werner@gmail.com>
2018-04-15 14:54:55 -0700

    working on consuming api

--- Lab18Magic/Lab18Magic/Models/Deck.cs
+++ Lab18Magic/Lab18Magic/Models/Deck.cs
@@ -7,8 +7,9 @@ namespace Lab18Magic.Models
 {
     public class Deck
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public Card Card { get; set; }
+        public List<Card> Cards { get; set; }
     }
 }
---
+++ Lab18Magic/Lab18Magic/Models/DeckViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18Magic.Models
+{
+    public class DeckViewModel
+    {
+        public List<Deck> decks;
+        public List<Card> cards;
+        public SelectList name;
+        public SelectList card;
+        public string DeckName { get; set; }
+    }
+}
