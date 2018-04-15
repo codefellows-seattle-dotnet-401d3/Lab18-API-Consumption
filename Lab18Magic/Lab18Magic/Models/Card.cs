b6ef79708ecedb12603998eb2ff20f691fa941cf
philipwerner <philip.r.werner@gmail.com>
2018-04-15 12:55:31 -0700

    working on models

---
+++ Lab18Magic/Lab18Magic/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18Magic.Models
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public string ManaCost { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Lab18Magic/Lab18Magic/Models/Deck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18Magic.Models
+{
+    public class Deck
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public Card Card { get; set; }
+    }
+}
